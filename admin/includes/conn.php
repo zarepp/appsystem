aae953d3386f80469fcb5c2825ef5d3dd9d48160
Zharif <zharifothman92@gmail.com>
2019-06-10 02:22:48 +0800

    Update config

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'apsystem');
+	$conn = new mysqli('35.225.202.254', 'root', 'opensesame', 'apsystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
