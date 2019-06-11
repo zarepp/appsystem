dc87bdc5ec3c55b959040fcb0055557109f0219b
Zharif <zharifothman92@gmail.com>
2019-06-11 22:39:23 +0800

    Update db config

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('35.225.202.254', 'root', 'opensesame', 'apsystem');
+	$conn = new mysqli('us-cdbr-iron-east-02.cleardb.net', 'b24b5bddd7cd09', 'd6cf68d7', 'heroku_df473506942c883');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
